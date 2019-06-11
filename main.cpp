41fdbab8ff1fee17a4e1b0cf7af9a56c298b8615
Damian Duchnowski <damianduchnowski1@gmail.com>
2019-06-11 15:50:56 +0200

    Preparation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type==sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
+    }
+
     return 0;
 }
