504af9aa3ce5a79e3079cd9c0ffcbdc3e7c65e81
Damian Duchnowski <damianduchnowski1@gmail.com>
2019-06-17 16:25:54 +0200

    Pipes spawn in smaller intervals and with more speed

--- World.cpp
+++ World.cpp
@@ -23,7 +23,7 @@ void World::checkCollisions()
 
 void World::createNewPipes()
 {
-    if (clock.getElapsedTime().asSeconds()>2.5) {
+    if (clock.getElapsedTime().asSeconds()>2.0) {
         Pipe tempPipe;
         pipes.push_back(tempPipe);
         clock.restart();
